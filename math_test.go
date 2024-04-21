d17dbd978384b804daa0bbef6db4a669b7238ff2
Pedro Ribeiro <pedroribeiro212@gmail.com>
2024-04-21 09:27:43 -0400

    ci: : :correção teste

--- math_test.go
+++ math_test.go
@@ -3,9 +3,9 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	total := soma(20, 20)
+	total := soma(15, 15)
 
 	if total != 30 {
-		t.Errorf("Resultado de soma é inválido: Resultado %d. Esperado: %d", total, 40)
+		t.Errorf("Resultado de soma é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
 }
